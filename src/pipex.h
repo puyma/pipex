4290ee8628657feb34abd247470723bff3635264
puyma <mpuig-ma@student.42barcelona.com>
2023-05-29 12:17:13 +0200

    header OK

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 20:42:24 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/05/26 13:14:52 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/05/29 11:40:02 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,8 @@
 int		file_to_fd(char *file, int fildes);
 int		fd_to_file(int fildes, char *file);
 void	ft_exit(int err_num);
+char	*ft_getenv(const char *name, const char **env);
+char	*ft_which(const char *exec, char *path);
+int		ft_execvpe(const char *cmd, char const *args[], char const *envp[]);
 
 #endif
