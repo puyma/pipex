0980e97a42b2444f09f29f28809472a013eb2c6d
puyma <mpuig-ma@student.42barcelona.com>
2023-05-29 12:17:04 +0200

    utils.c OK

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 12:29:19 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/05/26 12:38:57 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/05/29 12:11:15 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,51 @@ int	fd_to_file(int fildes, char *file)
 		write(fd, buf, 1);
 	close(fd);
 	return (0);
+}
+
+char	*ft_getenv(const char *name, const char **env)
+{
+	char	*variable;
+	int		i;
+
+	i = 0;
+	variable = ft_strjoin(name, "=");
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], "PATH=", ft_strlen("PATH=")) == 0)
+		{
+			free(variable);
+			return ((char *) env[i]);
+		}
+		++i;
+	}
+	free(variable);
+	variable = NULL;
+	return (variable);
+}
+
+char	*ft_which(const char *exec, char *path)
+{
+	size_t	dir_len;
+	char	*dir;
+	char	*filename;
+	char	*temp;
+
+	path = ft_strchr(path, '=') + 1;
+	while (path != NULL)
+	{
+		dir_len = ft_strchr(path, ':') - path;
+		dir = ft_strndup(path, dir_len);
+		temp = ft_strjoin(dir, "/");
+		filename = ft_strjoin(temp, exec);
+		free(temp);
+		free(dir);
+		if (access(filename, X_OK) == 0)
+			return (filename);
+		free(filename);
+		path = ft_strchr(path, ':');
+		if (path != NULL)
+			++path;
+	}
+	return (NULL);
 }
