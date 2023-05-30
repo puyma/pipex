2150691dfa671b29d5ae1f9e126efaa404f2d610
puyma <mpuig-ma@student.42barcelona.com>
2023-05-30 17:38:51 +0200

    rm ft_exit()

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 20:42:24 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/05/30 15:55:30 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/05/30 16:35:00 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@
 # define WR 1
 # define RD 0
 
-void	ft_exit(char *err_str, int err_num);
 char	*ft_getenv(const char *name, const char **env);
 char	*ft_which(const char *exec, char *path);
 int		ft_execvpe(const char *cmd, char const *args[], char const *envp[]);
