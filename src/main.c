752335eaddd396904fcaf676d958c4e3a3d80b1f
puyma <mpuig-ma@student.42barcelona.com>
2023-05-30 17:39:16 +0200

    redo check function

--- src/main.c
+++ src/main.c
@@ -6,67 +6,41 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:43:57 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/05/30 15:58:07 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/05/30 17:37:56 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void		ft_execute_command(char *argv, char **envp, int *fd);
-static int		check(int argc, char **argv, char **envp);
+static void		execute_command(char *argv, char **envp, int *fd);
+static int		check_args(int argc, char **argv, char **envp);
+static char		*cmd_path(char *argv, const char **envp);
 
 int	main(int argc, char **argv, char **envp)
 {
 	int		i;
 	int		fd;
+	int		outfd;
 	char	buf[1];
 
-	if (argc < 4)
-		return (1);
+	if (check_args(argc, argv, envp) != 0)
+		exit (1);
 	i = 2;
 	fd = open(argv[1], O_RDONLY);
 	while (argv[i + 1] != NULL)
 	{
 		dup2(fd, STDIN_FILENO);
 		close(fd);
-		ft_execute_command(argv[i++], envp, &fd);
+		execute_command(argv[i++], envp, &fd);
 	}
+	outfd = open(argv[argc - 1], O_CREAT | O_TRUNC | O_WRONLY, 0666);
 	while (read(fd, buf, 1) > 0)
-		write(STDOUT_FILENO, buf, 1);
+		write(outfd, buf, 1);
 	close(fd);
 	return (0);
 }
 
-static int	check(int argc, char **argv, char **envp)
-{
-	int		ret;
-	char	*cmd[3];
-	char	*temp;
-
-	ret = 0;
-	cmd[0] = NULL;
-	if (argc != 5 || access(argv[1], R_OK) == -1)
-		return (-1);
-	temp = argv[2];
-	if (ft_strchr(argv[2], ' ') != NULL)
-		temp = ft_strndup(argv[2], ft_strchr(argv[2], ' ') - argv[2]);
-	cmd[1] = ft_which(temp, ft_getenv("PATH=", (const char **) envp));
-	if (temp != argv[2])
-		free(temp);
-	temp = argv[3];
-	if (ft_strchr(argv[3], ' ') != NULL)
-		temp = ft_strndup(argv[3], ft_strchr(argv[3], ' ') - argv[3]);
-	cmd[2] = ft_which(temp, ft_getenv("PATH=", (const char **) envp));
-	if (temp != argv[3])
-		free(temp);
-	if (cmd[1] == NULL || cmd[2] == NULL)
-		ret = -1;
-	free(cmd[1]);
-	free(cmd[2]);
-	return (ret);
-}
-
-static void	ft_execute_command(char *argv, char **envp, int *fd)
+static void	execute_command(char *argv, char **envp, int *fd)
 {
 	int		fildes[2];
 	char	**cmd;
@@ -94,8 +68,47 @@ static void	ft_execute_command(char *argv, char **envp, int *fd)
 	*fd = fildes[RD];
 }
 
-void	ft_exit(char *err_str, int err_num)
+static int	check_args(int argc, char **argv, char **envp)
 {
-	perror(err_str);
-	exit(err_num);
+	int		i;
+	char	*cmd;
+
+	i = 2;
+	if (argc < 5)
+		return (write(2, "pipex: invalid number of arguments\n", 36));
+	if (open(argv[1], O_DIRECTORY) != -1 || access(argv[1], R_OK) != 0)
+		return (write(2, "pipex: invalid input file\n", 27));
+	while (argv[i] != NULL && i < argc - 1)
+	{
+		if (*argv[i] == '\0')
+			return (write(2, "pipex: invalid command\n", 24));
+		cmd = cmd_path(argv[i], (const char **) envp);
+		if (cmd == NULL)
+			return (write(2, "pipex: command not found\n", 25));
+		if (open(cmd, O_DIRECTORY) != -1 || access(cmd, X_OK) != 0)
+		{
+			free(cmd);
+			return (write(2, "pipex: invalid command\n", 24));
+		}
+		free(cmd);
+		++i;
+	}
+	return (0);
+}
+
+static char	*cmd_path(char *argv, const char **envp)
+{
+	char	*exec_name;
+	char	*cmd;
+
+	if (ft_strchr(argv, ' ') == NULL)
+		exec_name = argv;
+	else
+		exec_name = ft_strndup(argv, ft_strchr(argv, ' ') - argv);
+	cmd = ft_which(exec_name, ft_getenv("PATH=", envp));
+	if (ft_strchr(argv, ' ') != NULL)
+		free(exec_name);
+	if (cmd == NULL)
+		return (NULL);
+	return (cmd);
 }
